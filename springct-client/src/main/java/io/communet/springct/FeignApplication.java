07a3fa5b1f7b70d2013907dec3f8e3ee6787358f
LeeJohn <lidx@communet.io>
2017-11-23 15:52:30 +0800

    feign客户端 扫描

--- springct-client/src/main/java/io/communet/springct/FeignApplication.java
+++ springct-client/src/main/java/io/communet/springct/FeignApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"io.communet.springct.feign"})
 @EnableDiscoveryClient
 public class FeignApplication {
   public static void main(String[] args) {
